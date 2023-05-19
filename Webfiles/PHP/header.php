28e7da4aca3b1c62f782846348682bedbc4391fe
Malvina666 <marie.lemire@hotmail.com>
2023-05-19 12:02:55 +0200

    Ajout du fichier header.php

---
+++ Webfiles/PHP/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Notre selectionde parfums">
+    <link rel="stylesheet" href="./Webfiles/CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Open+Sans&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>RMH Parfums - Rasha, Marie, Hafssa </title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1>RMH PARFUMS</h1>
+            <ul>
+                <li><a href="../../index.php" title="redirection vers nos produits">Nos Produits</a></li>
+                <li><a href="#">à propos</a></li>
+                <li><a href="./Webfiles/PHP/Views/Users/connexion.php">Compte</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
